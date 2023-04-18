0891bcd9a942861deeb352dc2b6257e920b5f7d1
kuaile258890 <88901122+kuaile258890@users.noreply.github.com>
2023-04-18 16:25:54 +0800

    Add files via upload

---
+++ ChessAI.py
@@ -0,0 +1,201 @@
+import copy
+from ChessBoard import *
+
+
+class Evaluate(object):
+    # 棋子棋力得分
+    single_chess_point = {
+        'c': 989,   # 车
+        'm': 439,   # 马
+        'p': 442,   # 炮
+        's': 226,   # 士
+        'x': 210,   # 象
+        'z': 55,    # 卒
+        'j': 65536  # 将
+    }
+    # 红兵（卒）位置得分
+    red_bin_pos_point = [
+        [1, 3, 9, 10, 12, 10, 9, 3, 1],
+        [18, 36, 56, 95, 118, 95, 56, 36, 18],
+        [15, 28, 42, 73, 80, 73, 42, 28, 15],
+        [13, 22, 30, 42, 52, 42, 30, 22, 13],
+        [8, 17, 18, 21, 26, 21, 18, 17, 8],
+        [3, 0, 7, 0, 8, 0, 7, 0, 3],
+        [-1, 0, -3, 0, 3, 0, -3, 0, -1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    # 红车位置得分
+    red_che_pos_point = [
+        [185, 195, 190, 210, 220, 210, 190, 195, 185],
+        [185, 203, 198, 230, 245, 230, 198, 203, 185],
+        [180, 198, 190, 215, 225, 215, 190, 198, 180],
+        [180, 200, 195, 220, 230, 220, 195, 200, 180],
+        [180, 190, 180, 205, 225, 205, 180, 190, 180],
+        [155, 185, 172, 215, 215, 215, 172, 185, 155],
+        [110, 148, 135, 185, 190, 185, 135, 148, 110],
+        [100, 115, 105, 140, 135, 140, 105, 115, 110],
+        [115, 95, 100, 155, 115, 155, 100, 95, 115],
+        [20, 120, 105, 140, 115, 150, 105, 120, 20]
+    ]
+    # 红马位置得分
+    red_ma_pos_point = [
+        [80, 105, 135, 120, 80, 120, 135, 105, 80],
+        [80, 115, 200, 135, 105, 135, 200, 115, 80],
+        [120, 125, 135, 150, 145, 150, 135, 125, 120],
+        [105, 175, 145, 175, 150, 175, 145, 175, 105],
+        [90, 135, 125, 145, 135, 145, 125, 135, 90],
+        [80, 120, 135, 125, 120, 125, 135, 120, 80],
+        [45, 90, 105, 190, 110, 90, 105, 90, 45],
+        [80, 45, 105, 105, 80, 105, 105, 45, 80],
+        [20, 45, 80, 80, -10, 80, 80, 45, 20],
+        [20, -20, 20, 20, 20, 20, 20, -20, 20]
+    ]
+    # 红炮位置得分
+    red_pao_pos_point = [
+        [190, 180, 190, 70, 10, 70, 190, 180, 190],
+        [70, 120, 100, 90, 150, 90, 100, 120, 70],
+        [70, 90, 80, 90, 200, 90, 80, 90, 70],
+        [60, 80, 60, 50, 210, 50, 60, 80, 60],
+        [90, 50, 90, 70, 220, 70, 90, 50, 90],
+        [120, 70, 100, 60, 230, 60, 100, 70, 120],
+        [10, 30, 10, 30, 120, 30, 10, 30, 10],
+        [30, -20, 30, 20, 200, 20, 30, -20, 30],
+        [30, 10, 30, 30, -10, 30, 30, 10, 30],
+        [20, 20, 20, 20, -10, 20, 20, 20, 20]
+    ]
+    # 红将位置得分
+    red_jiang_pos_point = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 9750, 9800, 9750, 0, 0, 0],
+        [0, 0, 0, 9900, 9900, 9900, 0, 0, 0],
+        [0, 0, 0, 10000, 10000, 10000, 0, 0, 0],
+    ]
+    # 红相或士位置得分
+    red_xiang_shi_pos_point = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 60, 0, 0, 0, 60, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [80, 0, 0, 80, 90, 80, 0, 0, 80],
+        [0, 0, 0, 0, 0, 120, 0, 0, 0],
+        [0, 0, 70, 100, 0, 100, 70, 0, 0],
+    ]
+
+    red_pos_point = {
+        'z': red_bin_pos_point,
+        'm': red_ma_pos_point,
+        'c': red_che_pos_point,
+        'j': red_jiang_pos_point,
+        'p': red_pao_pos_point,
+        'x': red_xiang_shi_pos_point,
+        's': red_xiang_shi_pos_point
+    }
+
+    def __init__(self, team):
+        self.team = team
+
+    def get_single_chess_point(self, chess: Chess):
+        if chess.team == self.team:
+            return self.single_chess_point[chess.name]
+        else:
+            return -1 * self.single_chess_point[chess.name]
+
+    def get_chess_pos_point(self, chess: Chess):
+        red_pos_point_table = self.red_pos_point[chess.name]
+        if chess.team == 'r':
+            pos_point = red_pos_point_table[chess.row][chess.col]
+        else:
+            pos_point = red_pos_point_table[9 - chess.row][chess.col]
+        if chess.team != self.team:
+            pos_point *= -1
+        return pos_point
+
+    def evaluate(self, chessboard: ChessBoard):
+        point = 0
+        for chess in chessboard.get_chess():
+            point += self.get_single_chess_point(chess)
+            point += self.get_chess_pos_point(chess)
+        return point
+
+
+class ChessMap(object):
+    def __init__(self, chessboard: ChessBoard):
+        self.chess_map = copy.deepcopy(chessboard.chessboard_map)
+
+
+class ChessAI(object):
+    def __init__(self, computer_team):
+        self.team = computer_team
+        self.max_depth = 5
+        self.old_pos = [0, 0]
+        self.new_pos = [0, 0]
+        self.evaluate_class = Evaluate(self.team)
+
+    # def get_next_step(self, chessboard):
+    #     for chess in chessboard.get_chess():
+    #         if chess.team == self.team:
+    #             put_down_pos = chessboard.get_put_down_position(chess)
+    #             if len(put_down_pos) == 0:
+    #                 continue
+    #             return chess.row, chess.col, put_down_pos[0][0], put_down_pos[0][1]
+    #     return
+
+    def get_next_step(self, chessboard: ChessBoard):
+        self.old_pos = None
+        self.new_pos = None
+        self.alpha_beta(1, -1 * 0x3f3f3f3f, 0x3f3f3f3f, chessboard)
+        return self.old_pos[0], self.old_pos[1], self.new_pos[0], self.new_pos[1]
+
+    @staticmethod
+    def get_nxt_player(player):
+        if player == 'r':
+            return 'b'
+        else:
+            return 'r'
+
+    @staticmethod
+    def get_tmp_chessboard(chessboard, player_chess, new_row, new_col) -> ChessBoard:
+        tmp_chessboard = copy.deepcopy(chessboard)
+        tmp_chess = tmp_chessboard.chessboard_map[player_chess.row][player_chess.col]
+        tmp_chess.row, tmp_chess.col = new_row, new_col
+        tmp_chessboard.chessboard_map[new_row][new_col] = tmp_chess
+        tmp_chessboard.chessboard_map[player_chess.row][player_chess.col] = None
+        return tmp_chessboard
+
+    '''
+    TA提示： 
+
+    在下面的函数中，你可能需要这些信息：
+    self.max_depth              深度        
+    chess.team                  棋子所属队伍
+    self.team                   当前队伍
+    self.evaluate_class.evaluate(chessboard)                评估函数用法示例
+    chessboard.get_put_down_position(chess)                 获取chess可下位置（详见ChessBoard.py）
+    chessboard.chessboard_map                               棋盘（详见ChessBoard.py）
+    chessboard.chessboard_map[x][y].update_position(x, y)   在你执行了..._map[x][y]=..._map[x2][y2]之后记得补上这句（参考Chessboard.move_chess）
+
+    调用：见第157行。 原代码中直接在该剪枝函数中更新落子位置，即get_next_step中提及的self.old_pos以及self.new_pos。
+
+    ！请务必保证self.old_pos以及self.new_pos被更新（详见get_next_step）！
+    ！注意查看其他文件获取更多信息！
+    ！现在来重写你的alpha-beta剪枝函数吧！
+
+    '''
+    def alpha_beta(self, depth, a, b, chessboard: ChessBoard):
+        #以下是一些没用但能让项目跑起来的代码
+        chess = chessboard.get_chess()[0]
+        new_pos = chessboard.get_put_down_position(chess)[0]
+        self.old_pos = [chess.row, chess.col]
+        self.new_pos = new_pos
+        return a
